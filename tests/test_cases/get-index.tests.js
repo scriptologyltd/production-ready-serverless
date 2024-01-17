0465373272c35eb281cdd51143c80c176cf81f32
Andrew Whitehouse <andrew@whitehouse.org.uk>
2024-01-17 17:40:42 +0000

    Fixes 'Should return the index page with 8 restaurants' test

--- tests/test_cases/get-index.tests.js
+++ tests/test_cases/get-index.tests.js
@@ -1,7 +1,10 @@
 const cheerio = require('cheerio')
 const when = require('../steps/when')
+const { init } = require('../steps/init')
 
 describe(`When we invoke the GET / endpoint`, () => {
+  beforeAll(async () => await init())
+
   it(`Should return the index page with 8 restaurants`, async () => {
     const res = await when.we_invoke_get_index()
 
